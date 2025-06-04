0982cac06fc7fb1fba7dc0dc253098ce20d2ba96
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-04 15:40:11 +0300

    kangaroo controller done

--- src/main/java/com/workintech/zoo/controller/KangarooController.java
+++ src/main/java/com/workintech/zoo/controller/KangarooController.java
@@ -1,4 +1,57 @@
 package com.workintech.zoo.controller;
 
+import com.workintech.zoo.entity.Kangaroo;
+import com.workintech.zoo.exceptions.ZooException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/kangaroos")
 public class KangarooController {
+
+    private Map<Integer, Kangaroo> kangaroos = new HashMap<>();
+
+    @GetMapping
+    public Collection<Kangaroo> getAll() {
+        return kangaroos.values();
+    }
+
+    @GetMapping("/{id}")
+    public Kangaroo getById(@PathVariable int id) {
+        Kangaroo kangaroo = kangaroos.get(id);
+        if (kangaroo == null) {
+            throw new ZooException("Kangaroo not found", HttpStatus.NOT_FOUND);
+        }
+        return kangaroo;
+    }
+
+    @PostMapping
+    public Kangaroo create(@RequestBody Kangaroo kangaroo) {
+        if (kangaroo.getId() == 0 || kangaroo.getName() == null) {
+            throw new ZooException("Invalid kangaroo data", HttpStatus.BAD_REQUEST);
+        }
+        kangaroos.put(kangaroo.getId(), kangaroo);
+        return kangaroo;
+    }
+
+    @PutMapping("/{id}")
+    public Kangaroo update(@PathVariable int id, @RequestBody Kangaroo kangaroo) {
+        if (!kangaroos.containsKey(id)) {
+            throw new ZooException("Kangaroo not found", HttpStatus.NOT_FOUND);
+        }
+        kangaroo.setId(id);
+        kangaroos.put(id, kangaroo);
+        return kangaroo;
+    }
+
+    @DeleteMapping("/{id}")
+    public Kangaroo delete(@PathVariable int id) {
+        Kangaroo removed = kangaroos.remove(id);
+        if (removed == null) {
+            throw new ZooException("Kangaroo not found", HttpStatus.NOT_FOUND);
+        }
+        return removed;
+    }
 }
