1910ddaa91fa9b8c9e6ea38272c2b5a179960ac0
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-04 15:36:03 +0300

    koala class done with lombok annotations

--- src/main/java/com/workintech/zoo/entity/Koala.java
+++ src/main/java/com/workintech/zoo/entity/Koala.java
@@ -1,4 +1,14 @@
 package com.workintech.zoo.entity;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Koala {
+    private int id;
+    private String name;
+    private double sleepHour;
+    private double weight;
+    private String gender;
 }
