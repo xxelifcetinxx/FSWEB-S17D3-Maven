c62cf7143b0d6012fad8d645c041dc5183a0f236
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-04 15:44:16 +0300

    zoo error response done

--- src/main/java/com/workintech/zoo/exceptions/ZooErrorResponse.java
+++ src/main/java/com/workintech/zoo/exceptions/ZooErrorResponse.java
@@ -1,4 +1,14 @@
 package com.workintech.zoo.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ZooErrorResponse {
+    private int status;
+    private String message;
+    private long timestamp;
 }
