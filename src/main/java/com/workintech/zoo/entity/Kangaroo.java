9f39f7ffcdbc9b88a4e74c642490391cc8e87f1c
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-04 15:36:49 +0300

    kangaroo class done with lombok annotations

--- src/main/java/com/workintech/zoo/entity/Kangaroo.java
+++ src/main/java/com/workintech/zoo/entity/Kangaroo.java
@@ -1,4 +1,15 @@
 package com.workintech.zoo.entity;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Kangaroo {
+    private int id;
+    private String name;
+    private double height;
+    private double weight;
+    private String gender;
+    private boolean isAggressive;
 }
